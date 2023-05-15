a720148799315fef605c457ff182fa007c8fa46d
Brad Langshaw <bradlangshaw@users.noreply.github.com>
2023-05-15 15:57:06 -0400

    Update globals.js

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,4 @@
 module.exports = {
   // db: 'mongodb://localhost/comp2106-w2017' // local mongodb (no auth)
-   db: 'mongodb://bradlangs:Linkbrad1!@ds135800.mlab.com:35800/bradlangshawtech',
+   db: 'mongodb://bradlangs:test@ds135800.mlab.com:35800/bradlangshawtech',
 };
